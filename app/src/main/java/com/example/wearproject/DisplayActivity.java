195e84d32adf95ec650da2e7d2927fed756dd3c2
Terki Adel <terki.adel@yahoo.com>
2020-01-18 23:10:50 +0100

    JsonArrayRequest

--- app/src/main/java/com/example/wearproject/DisplayActivity.java
+++ app/src/main/java/com/example/wearproject/DisplayActivity.java
@@ -15,6 +15,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.BasicNetwork;
 import com.android.volley.toolbox.DiskBasedCache;
 import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.wearapplication.Message;
 
@@ -59,41 +60,40 @@ public class DisplayActivity extends Activity {
         public ArrayList<com.example.wearapplication.Message> getMessages(){
 
         final ArrayList<com.example.wearapplication.Message> messages = new ArrayList<com.example.wearapplication.Message>();
-            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, Constants.URL_GET,null,
-                    new Response.Listener<JSONObject>() {
+            Log.d("success 1", "get_message");
+            JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, Constants.URL_GET,null,
+                    new Response.Listener<JSONArray>() {
                         @Override
-                        public void onResponse(JSONObject response) {
+                        public void onResponse(JSONArray response) {
                             try {
-                                JSONArray jsonArray = response.getJSONArray(" ");
-                                if(jsonArray != null )
-                                    Log.d("getting JSON Array","success");
-                                else
-                                    Log.d("getting JSON Array","failure");
-
-                                for(int i=0;i < jsonArray.length();i++){
-                                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                    Log.d("student_message",jsonObject.getString("student_message"));
+                                for (int i = 0; i < response.length(); i++) {
+                                    JSONObject message =  response.getJSONObject(i);
+                                    Log.d("Response", message.getString("student_message"));
                                     HashMap coordinates = new HashMap();
-                                    coordinates.put("gps_lat",jsonObject.getDouble("gps_lat"));
-                                    coordinates.put("gps_long",jsonObject.getDouble("gps_long"));
+                                    coordinates.put("gps_lat", message.getDouble("gps_lat"));
+                                    coordinates.put("gps_long", message.getDouble("gps_long"));
                                     messages.add(new Message(
-                                     jsonObject.getInt("id"),
-                                     jsonObject.getInt("student_id"),
-                                     jsonObject.getString("student_message"),
-                                     coordinates
+                                            message.getInt("id"),
+                                            message.getInt("student_id"),
+                                            message.getString("student_message"),
+                                            coordinates
                                     ));
+                                    Log.d("success","after adding to messages");
                                 }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
+                             } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                         }
                     },
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                        }
+                            //Log.d("Error.Response", error.toString());
+                         }
                     });
+            Log.d("success","return statement");
             RequestHandler.getInstance(this).addToRequestQueue(request);
+            Log.d("success","return statement");
             return messages;
         }
 
